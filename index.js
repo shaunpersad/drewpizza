8945c55cd64b07e6b074940518fc35deadfd0456
shaunpersad <shaunpersad@gmail.com>
2016-11-24 00:42:06 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,86 @@
+"use strict";
+
+var token = process.env.SLACK_BOT_TOKEN || '';
+var channelName = 'nyc-office';
+var sucker = 'drewpnyc';
+var suckerName = 'Drew';
+
+var RtmClient = require('@slack/client').RtmClient;
+var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+
+var rtm = new RtmClient(token);
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+
+    var currentlyTrackingDate = null;
+    var pizzaCounter = 0;
+
+    var channels = rtmStartData.channels || [];
+    var channelId = null;
+    
+    channels.forEach(function(channel) {
+        if (channel.name === channelName) {
+            channelId = channel.id;
+        }
+    });
+
+    var logPizza = function() {
+
+        var now = new Date();
+        var date = `${now.getFullYear()}-${now.getMonth()}-${now.getDate()}`;
+
+        if (date !== currentlyTrackingDate) {
+            currentlyTrackingDate = date;
+            pizzaCounter = 0;
+        }
+
+        pizzaCounter++;
+
+        if (pizzaCounter >= 15) {
+
+            rtm.sendMessage(`<@channel>, you guys need to calm down RIGHT NOW.`, channelId);
+
+        } else if (pizzaCounter >= 10) {
+
+            rtm.sendMessage(`<@${sucker}|${suckerName}>, I hope you ordered that pizza.`, channelId);
+
+        } else if (pizzaCounter >= 5) {
+
+            rtm.sendMessage(`<@${sucker}|${suckerName}>, pizza?`, channelId);
+        }
+    };
+
+
+    rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
+
+        if (message.channel === channelId &&
+            message.type === 'message' &&
+            !message.subtype) {
+
+            var text = message.text || '';
+
+            if (text.indexOf(':pizza:') !== -1) {
+
+                logPizza();
+            }
+        }
+    });
+
+    rtm.on(RTM_EVENTS.REACTION_ADDED, function handleRtmReaction(message) {
+
+        if (message.item.channel === channelId &&
+            message.type === 'reaction_added') {
+
+            var reaction = message.reaction || '';
+
+            if (reaction === 'pizza') {
+
+                logPizza();
+            }
+        }
+    });
+
+});
+
+rtm.start();
